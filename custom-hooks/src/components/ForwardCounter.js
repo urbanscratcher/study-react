9dfa673a7b44c67e8c44bc2b261918ded64ad01a
Joun <urbanscratcher@gmail.com>
2023-04-17 01:43:16 +0900

    course done

---
+++ custom-hooks/src/components/ForwardCounter.js
@@ -0,0 +1,9 @@
+import Card from "./Card";
+import useCounter from "../hooks/use-counter";
+
+const ForwardCounter = () => {
+  const counter = useCounter(true);
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
